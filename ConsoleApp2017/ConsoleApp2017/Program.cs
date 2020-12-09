1e360a7e2f378491fb099a3cbd3ee1d9e85c7d32
Henry Yu <henry.yu@ceridian.com>
2020-12-09 10:06:33 -0500

    test

---
+++ ConsoleApp2017/ConsoleApp2017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ConsoleApp2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blah = "blah";
+            Console.WriteLine(JsonConvert.SerializeObject(blah));
+            Console.ReadLine();
+        }
+    }
+}
