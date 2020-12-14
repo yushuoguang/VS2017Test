30c9bd9a80c9a12d048d828eff26b54953b8ccce
Henry Yu <henry.yu@ceridian.com>
2020-12-14 11:39:04 -0500

    Add unit test

---
+++ ConsoleApp2017/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("506871c1-253c-497e-b0d2-42c10b145b75")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsoleApp2017/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestProject
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            string a = "a";
+            Assert.AreEqual("a", a);
+        }
+    }
+}
